77d19c757eb9f8a15497e2ea54b9c32630710bc9
peach-on-the-way <peachinhome@gmail.com>
2025-09-01 21:12:15 +0700

    Document player.rs

--- src/player.rs
+++ src/player.rs
@@ -1,21 +1,30 @@
+//! Handles player
+
 use crate::prelude::*;
 
+/// Global resource about main player
 pub struct Player {
+    /// The player's entity id
     pub id: Entity,
+    /// The primary weapon
     pub primary_weapon: Weapon,
 }
 
+/// Weapons
 pub enum Weapon {
+    /// Default weapon
     Stick,
 }
 
 impl Weapon {
+    /// Get base delay for each weapon
     pub fn base_delay(&self) -> Duration {
         match self {
             Weapon::Stick => Duration::from_millis(700),
         }
     }
 
+    /// Get base damage for each weapon
     pub fn base_damage(&self) -> i32 {
         match self {
             Weapon::Stick => 4,
@@ -31,10 +40,12 @@ impl std::fmt::Display for Weapon {
     }
 }
 
+/// Basic WASD player movement.
 pub fn player_movement_system(move_timer: &mut Timer, arena: &Vec2i32, player_id: Entity, inputs: &Inputs, collider_grid: &mut ColliderGrid, entities: &mut Entities, positions: &mut Components<Vec2i32>) {
     let mut moved = false;
     let player_pos = positions.get_mut(entities, player_id).unwrap();
     let mut new_pos = *player_pos;
+    // move_timer make sures the player doesn't move too fast
     if move_timer.finished() && inputs.pressed.contains(&KeyCode::Char('w')) && player_pos.1 > -arena.1 {
         new_pos.1 = player_pos.1.saturating_sub(1);
         moved = true;
@@ -61,9 +72,11 @@ pub fn player_movement_system(move_timer: &mut Timer, arena: &Vec2i32, player_id
     }
 }
 
+/// Weapon system
 pub fn player_weapon_system(arena: &Vec2i32, player: &Player, weapon_timer: &mut Timer, collider_grid: &ColliderGrid, draw_events: &mut Events<Draw>, damage_events: &mut Events<Damage>, entities: &Entities, inputs: &Inputs, positions: &Components<Vec2i32>) {
     let player_pos = *positions.get(entities, player.id).unwrap();
 
+    // Control the direction in which the weapon is activated
     let mut dir = (0, 0);
     if inputs.pressed.contains(&KeyCode::Up) {
         dir.1 -= 1
@@ -84,6 +97,7 @@ pub fn player_weapon_system(arena: &Vec2i32, player: &Player, weapon_timer: &mut
         Weapon::Stick => {
             let attack_mid = (player_pos.0 + dir.0 * 2, player_pos.1 + dir.1 * 2);
             let attack_top_left = (attack_mid.0 - 1, attack_mid.1 - 1);
+            // Area of attack effect
             draw_events.push(Draw { draw_info: DrawInfo { sprite: Sprite { char: '.', ..Default::default() }, shape: Shape::Rectangle { w: 3, h: 3 } }, position: attack_top_left, timer: Timer::new(Duration::from_millis(50)) });
             for x in 0..3 {
                 for y in 0..3 {
@@ -100,6 +114,7 @@ pub fn player_weapon_system(arena: &Vec2i32, player: &Player, weapon_timer: &mut
     weapon_timer.reset();
 }
 
+/// Detects player dying and set `player_dead` state
 pub fn player_killed_system(kill_events: &Events<Kill>, player_dead: &mut bool, player: &Player) {
     for kill in kill_events {
         if player.id == kill.target {
